8c982c1a7127354f97f3e51b737ef652adad4a2f
th15t13 <854052880@qq.com>
2022-10-07 15:51:40 +0800

    Implement signing up.

--- app/src/main/java/com/example/comp9900_commercialize/LoginActivity.java
+++ app/src/main/java/com/example/comp9900_commercialize/LoginActivity.java
@@ -1,14 +1,84 @@
 package com.example.comp9900_commercialize;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.example.comp9900_commercialize.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private FirebaseAuth auth;;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        init();
+        setListeners();
     }
+
+    private void setListeners(){
+        binding.btLogin.setOnClickListener(v -> {
+            if (isValidInput()) {
+
+                login();
+            }});
+        binding.tvForgotPassword.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), ForgotPasswordActivity.class)));
+        binding.tvSignUp.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
+    }
+
+    private void init(){
+        auth= FirebaseAuth.getInstance();
+    }
+
+    private void login() {
+        String email = binding.etEmail.getText().toString().trim();
+        String pwd = binding.etPassword.getText().toString().trim();
+        auth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = auth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            showToast("Authentication failed.");
+                        }
+                    }
+                });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message,Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isValidInput() {
+        if (binding.etEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter email");
+            return false;
+        } else if (binding.etPassword.getText().toString().trim().isEmpty()) {
+            showToast("Enter password");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
