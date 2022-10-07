cabd25a3753661b98e034eb54140ca4800aeb5b2
MSIK <767831805@qq.com>
2022-10-07 13:55:19 +0800

    OtherProfile,Following,Collection Page

--- app/src/main/java/com/example/comp9900_commercialize/LoginActivity.java
+++ app/src/main/java/com/example/comp9900_commercialize/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.comp9900_commercialize;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvSignUp = findViewById(R.id.tv_sign_up);
+        // listen for action of the textview 'Don't have and  account? Sign up here'
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // jump from LoginActivity.java to UserTypeActivity.java
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
