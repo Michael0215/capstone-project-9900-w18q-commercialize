7b2c39150cd622efacdc8fe2534eef9be82bd4e0
th15t13 <854052880@qq.com>
2022-10-17 20:47:27 +0800

    Implemented adding ingredients and procedures.

--- app/src/main/java/com/example/comp9900_commercialize/AddRecipeSub1Activity.java
+++ app/src/main/java/com/example/comp9900_commercialize/AddRecipeSub1Activity.java
@@ -1,12 +1,17 @@
 package com.example.comp9900_commercialize;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 
@@ -15,13 +20,16 @@ import com.example.comp9900_commercialize.databinding.ActivityAddRecipeSub1Bindi
 import java.util.ArrayList;
 import java.util.List;
 
-public class AddRecipeSub1Activity extends AppCompatActivity {
+public class AddRecipeSub1Activity extends AppCompatActivity implements View.OnClickListener {
     private ActivityAddRecipeSub1Binding binding;
+    private LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAddRecipeSub1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        linearLayout = binding.llIngredients;
         setListeners();
     }
 
@@ -35,6 +43,34 @@ public class AddRecipeSub1Activity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), AddRecipeActivity.class)));
         binding.btNext.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), AddRecipeSub2Activity.class)));
+        binding.btAddIngredient.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        addView();
+    }
+
+    private void addView() {
+
+        View ingredientView = getLayoutInflater().inflate(R.layout.row_add_ingredient, null, false);
+        EditText ingredientName = (EditText)ingredientView.findViewById(R.id.et_ingredient_name);
+        EditText amounts = (EditText)ingredientView.findViewById(R.id.et_amounts);
+        ImageView remove = (ImageView)ingredientView.findViewById(R.id.iv_remove_ingredient);
+
+        remove.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                removeView(ingredientView);
+            }
+        });
+
+        linearLayout.addView(ingredientView);
+
+    }
+
+    private void removeView(View view) {
+        linearLayout.removeView(view);
+    }
 }
--- app/src/main/java/com/example/comp9900_commercialize/AddRecipeSub2Activity.java
+++ app/src/main/java/com/example/comp9900_commercialize/AddRecipeSub2Activity.java
@@ -1,21 +1,29 @@
 package com.example.comp9900_commercialize;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.comp9900_commercialize.databinding.ActivityAddRecipeSub2Binding;
 
 
 
-public class AddRecipeSub2Activity extends AppCompatActivity {
+public class AddRecipeSub2Activity extends AppCompatActivity implements View.OnClickListener {
     private ActivityAddRecipeSub2Binding binding;
+    private LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAddRecipeSub2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        linearLayout = binding.llProcedures;
         setListeners();
     }
 
@@ -28,5 +36,34 @@ public class AddRecipeSub2Activity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), AddRecipeSub1Activity.class)));
         binding.btPublish.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), MainActivity.class)));
+        binding.btAddProcedure.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        addView();
+    }
+
+    private void addView() {
+
+        View procedureView = getLayoutInflater().inflate(R.layout.row_add_procedure, null, false);
+        EditText procedureDescription = (EditText)procedureView.findViewById(R.id.et_procedure_description);
+        ImageView image = (ImageView) procedureView.findViewById(R.id.iv_procedure_photo);
+        ImageView remove = (ImageView)procedureView.findViewById(R.id.iv_remove_procedure);
+
+        remove.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                removeView(procedureView);
+            }
+        });
+
+        linearLayout.addView(procedureView);
+
+    }
+
+    private void removeView(View view) {
+        linearLayout.removeView(view);
     }
 }
