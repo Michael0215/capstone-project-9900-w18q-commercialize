9045e5ba3e82ac958b89b9e131b45d12a29a93cd
karldeng1998 <z5348431@ad.unsw.edu.au>
2022-10-24 15:52:53 +0800

    CollectionClass used for collectionActivity

---
+++ app/src/main/java/com/example/comp9900_commercialize/bean/Collection.java
@@ -0,0 +1,22 @@
+package com.example.comp9900_commercialize.bean;
+
+import java.util.List;
+
+public class Collection {
+
+    public List<String> collectionList;
+
+    public Collection(List<String> collectionList) {
+        this.collectionList = collectionList;
+    }
+
+    public List<String> getCollectionList() {
+        return collectionList;
+    }
+
+    public void setCollectionList(List<String> collectionList) {
+        this.collectionList = collectionList;
+    }
+    public Collection(){}
+
+}
