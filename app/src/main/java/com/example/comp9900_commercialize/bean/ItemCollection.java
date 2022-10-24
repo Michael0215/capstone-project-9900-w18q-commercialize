cada7a7c2641761cb2830d753c2088e563580269
z5277454 <100696819+z5277454@users.noreply.github.com>
2022-10-24 21:24:04 +0800

    Collection function implemented

---
+++ app/src/main/java/com/example/comp9900_commercialize/bean/ItemCollection.java
@@ -0,0 +1,11 @@
+package com.example.comp9900_commercialize.bean;
+
+import android.graphics.Bitmap;
+
+public class ItemCollection {
+    public String id;
+    public Bitmap icon;
+    public String title;
+    public Bitmap avatar;
+    public String tv_contributor_name;
+}
